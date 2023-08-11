2654867e18305a7128da81ea9e6e7415834e5e96
Sarathkp87 <125114316+Sarathkp87@users.noreply.github.com>
2023-08-11 15:54:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express =require('express');
+
+const cors=require('cors');
+
+const logic =require('./services/logic')
+
+//server creation
+const server =express()
+
+server.use(cors({
+    origin:'http://localhost:3000'
+}))
+
+server.use(express.json())
+
+server.use.listen(8000,()=>{
+    console.log('listnening on the port');
+})
+
+//get all products
+server.get('allProducts',(req,res)=>{
+    logic.allProducts().then((result)=>{
+        res.status(result.statusCode).json(result)
+    })
+})
+
